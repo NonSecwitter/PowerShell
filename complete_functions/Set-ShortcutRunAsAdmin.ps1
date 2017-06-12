9e98091fc2ed3e33207258195d7bdd7e1c772324
Jonathon Anderson <jpanderson0@gmail.com>
2017-06-12 13:32:37 -0500

    Create Set-ShortcutRunAsAdmin.ps1

--- complete_functions/Set-ShortcutRunAsAdmin.ps1
+++ complete_functions/Set-ShortcutRunAsAdmin.ps1
@@ -1,3 +1,16 @@
+<#
+.SYNOPSIS
+Sets the "RunAs Administrator" flag on specified .LNK files.
+.PARAMETER FileName
+The .LNK file to modify
+.PARAMETER RunAss
+Boolean value to indicate whether .LNK should "RunAss Administrator"
+.EXAMPLE
+Set-ShortcutRunAsAdmin -FileName "C:\foo\bar.lnk" -RunAs $true
+.EXAMPLE
+Get-ChildItem -LiteralPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools" -Filter *.LNK | Set-ShortcutRunAsAdmin
+#>
+
 Function Set-ShortcutRunAsAdmin
 {
     [CmdletBinding()]
