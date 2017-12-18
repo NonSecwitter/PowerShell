44a498edc705a1e38f8dc78830f42b86edd982d5
Jonathon Anderson <jpanderson0@gmail.com>
2017-12-18 14:32:20 -0600

    Create Template.ps1

---
+++ Template.ps1
@@ -0,0 +1,23 @@
+<#
+    .AUTHOR
+        Jonathon Anderson
+    .COPYRIGHT 
+        Jonathon Anderson
+
+    .SYNOPSIS
+    .PARAMETER
+    .EXAMPLE
+    .TODO
+#>
+
+#function Function-Name
+#{
+    [CmdletBinding()]
+    Param ()
+
+    BEGIN {}
+
+    PROCESS {}
+
+    END {}
+#}
